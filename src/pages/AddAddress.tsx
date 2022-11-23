2b46334f60f17bf28d8a201e5b5c07c695caaefa
LUIS GERARDO AGUILAR GARCIA <eagledluis.game@gmail.com>
2022-11-23 12:14:37 -0500

    Menu Fix

--- src/pages/AddAddress.tsx
+++ src/pages/AddAddress.tsx
@@ -1,10 +1,8 @@
 import { AddAddress } from "../components/AddAddress";
-import { Menu } from "../components/Menu";
 
 export default () => {
   return (
     <div className="UserPanelF_MainContainer">
-      <Menu />
       <AddAddress />
     </div>
   );
