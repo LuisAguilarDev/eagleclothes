558b6ea530140fd1295ad15be771a93df2ebf314
LUIS GERARDO AGUILAR GARCIA <eagledluis.game@gmail.com>
2022-12-05 20:42:46 -0500

    solve error on empty cart

--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -56,7 +56,7 @@ export const Login = ({ close }: Props) => {
         }
         setToken(res.data.token);
         setName(res.data.user.name);
-        if (cart.length ?? cart.length > 0) {
+        if (cart?.length) {
           cart.forEach((item: productType) => {
             addToCart(evt, [item]);
           });
